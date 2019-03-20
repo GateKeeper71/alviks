3dfd69256452089a58ee9f018656a22fdd117f51
Max Trewhitt <max.trewhitt@osolo.se>
2019-03-20 16:25:04 +0100

    Add: New hero grid

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,15 @@
-import { AnimateCurve } from './ux/animate';
-import { AnimateJumbotron } from './ux/animate';
-import { Navbar } from './ux/navbar';
-import { Tip } from './ux/tip';
-import './custom/search';
-import makeSlider from './custom/slider';
+import "./custom/search";
+import makeSlider from "./custom/slider";
 
 // JS Goes here - ES6 supported
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
+	window.netlifyIdentity.on("init", user => {
+		if (!user) {
+		window.netlifyIdentity.on("login", () => {
+			document.location.href = "/admin/";
+		});
+		}
+	});
 }
-
-const animateCurve = new AnimateCurve();
-const animateJumbotron = new AnimateJumbotron();
-const navbar = new Navbar();
-const tooltip = new Tip();
 
 makeSlider();
