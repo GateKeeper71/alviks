0451bc58a6c33a8466f7d7b317919e2a864f0572
Max Trewhitt <max.trewhitt@osolo.se>
2019-03-27 12:16:30 +0100

    Rename React files to .jsx

--- src/js/custom/menus.js
+++ src/js/custom/menus.js
@@ -1,5 +1,6 @@
 $(() => {
 	$(".c-fixedHeader__sideMenu .icon-link").click((e) => {
+		$(".c-fixedHeader__sideMenu .icon-link").removeClass("toggle");
 		$(e.target).closest(".icon-link").toggleClass("toggle");
 	});
 });
--- webpack.conf.js
+++ webpack.conf.js
@@ -2,36 +2,38 @@ import webpack from "webpack";
 import path from "path";
 
 export default {
-  module: {
-    loaders: [
-      {
-        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
-      },
-      {
-        loader: "babel-loader",
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        query: {cacheDirectory: true}
-      }
-    ]
-  },
+	resolve: { extensions: [".jsx", ".js", ".json"] },
+	module: {
+		loaders: [{
+				test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file-loader?name=/[hash].[ext]"
+			},
+			{
+				loader: "babel-loader",
+				test: /\.(js|jsx)?$/,
+				exclude: /node_modules/,
+				query: {
+					cacheDirectory: true
+				}
+			}
+		]
+	},
 
-  plugins: [
-    new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
-    })
-  ],
+	plugins: [
+		new webpack.ProvidePlugin({
+			"fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+		})
+	],
 
-  context: path.join(__dirname, "src"),
-  entry: {
-    app: ["./js/app"],
-    cms: ["./js/cms"]
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
-  },
-  externals:  [/^vendor\/.+\.js$/]
+	context: path.join(__dirname, "src"),
+	entry: {
+		app: ["./js/app"],
+		cms: ["./js/cms"]
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		publicPath: "/",
+		filename: "[name].js"
+	},
+	externals: [/^vendor\/.+\.js$/]
 };
